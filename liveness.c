e4b7bcbd19c2bb04822d7a3563b2700d595bf370
Li Haoyu <Guita_r@sjtu.edu.cn>
2016-12-15 20:46:12 +0800

    finish flowgraph

--- liveness.c
+++ liveness.c
@@ -11,6 +11,8 @@
 #include "liveness.h"
 #include "table.h"
 
+static G_table in, out;
+
 Live_moveList Live_MoveList(G_node src, G_node dst, Live_moveList tail) {
 	Live_moveList lm = (Live_moveList) checked_malloc(sizeof(*lm));
 	lm->src = src;
@@ -22,7 +24,62 @@ Live_moveList Live_MoveList(G_node src, G_node dst, Live_moveList tail) {
 
 Temp_temp Live_gtemp(G_node n) {
 	//your code here.
+
 	return NULL;
+}
+
+static Temp_tempList minus(Temp_tempList a, Temp_tempList b) {
+	Temp_tempList i, j, r = NULL;
+	for (i = a; i; i = i->tail) {
+		int found = 0;
+		for (j = b; j; j = j->tail)
+			if (i->head.num == j->head.num) {
+				found = 1;
+				break;
+			}
+		if (!found) r = Temp_TempList(i->head, r);
+	}
+	return r;
+}
+
+static Temp_tempList plus(Temp_tempList a, Temp_tempList b) {
+	Temp_tempList i, j, r = NULL;
+	for (i = a; i; i = i->tail)
+		r = Temp_TempList(i->head, NULL);
+	for (i = b; i; i = i->tail) {
+		int found = 0;
+		for (j = a; j; j = j->tail)
+			if (i->head->num == j->head->num) {
+				found = 1;
+				break;
+			}
+		if (!found) r = Temp_TempList(i->head, r);
+	}
+	return r;
+}
+
+static void Liveness_initial(G_graph flow) {
+	in = G_empty();
+	out = G_empty();
+	G_nodeList nodes = G_nodes(flow), n;
+	for (n = nodes; n; n = n->tail) {
+		G_enter(in, n->head, NULL);
+		G_enter(out, n->head, NULL);
+	}
+}
+
+static void Liveness_Analysis(G_graph flow) {
+	Liveness_initial(flow);
+
+	G_nodeList nodes = G_nodes(flow), i;
+	for (i = nodes; i; i = i->tail) {
+		G_node n = i->head;
+		Temp_tempList def = FG_def(n);
+		Temp_tempList use = FG_use(n);
+
+
+	}
+
 }
 
 struct Live_graph Live_liveness(G_graph flow) {
